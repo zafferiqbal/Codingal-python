f040992f2de28ade900d9f756b947f7b78247eb4
zafferiqbal <101118051+zafferiqbal@users.noreply.github.com>
2026-02-22 02:32:29 +0530

    Add files via upload

---
+++ M1L7A1.py
@@ -0,0 +1,41 @@
+# Python program to illustrate the use
+
+# of 'is' identity operator
+
+
+x = 5
+
+if (type(x) is int):
+
+    print("true")
+
+else:
+
+    print("false")
+
+ 
+x = 5.5
+
+if (type(x) is not float):
+
+    print("true")
+
+else:
+
+    print("false")
+
+ 
+x = 20
+
+y = 20
+
+if (x is y):
+
+    print("x & y SAME identity")
+
+ 
+y = 30
+
+if (x is not y):
+
+    print("x & y have DIFFERENT identity")
