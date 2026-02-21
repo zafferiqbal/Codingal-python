b3ebdbface06f4369a5073e60a6a48d3a569d56c
zafferiqbal <101118051+zafferiqbal@users.noreply.github.com>
2026-02-21 15:17:48 +0530

    Add files via upload

---
+++ M1A1.py
@@ -0,0 +1,12 @@
+# Printing a greeting message
+print("Hello, world!")
+
+# Printing my name
+print("My name is Zaffer Iqbal.")
+
+# Printing a fun fact about myself
+# print("I love playing soccer.")
+print("I love playing Football.")
+
+# Performing and printing a simple calculation
+print("The result of 8 + 2 is:", 8 + 2)
