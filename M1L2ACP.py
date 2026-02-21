b291d18424b6c986d06c6be02ba5e30f44368f26
zafferiqbal <101118051+zafferiqbal@users.noreply.github.com>
2026-02-21 15:34:20 +0530

    Add files via upload

---
+++ M1L2ACP.py
@@ -0,0 +1,12 @@
+print("Raj is asking for his friends' birthdays...")
+friend1 = "Aman: 12th Jan"
+friend2 = "Sita: 5th March"
+friend3 = "Karan: 22nd July"
+friend4 = "Riya: 30th Sept"
+friend5 = "Rahul: 15th Dec"
+
+print(friend1)
+print(friend2)
+print(friend3)
+print(friend4)
+print(friend5)
